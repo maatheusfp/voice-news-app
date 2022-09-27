a4d782bc7d55bad31526eaa1e4e0fde82bc72124
Matheus PInheiro <matheus.pinheirofer@gmail.com>
2022-09-27 15:12:05 -0300

    Installed the dependecies and adjusts to starts

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css';
+
+export const Home: React.FC =() => {
+    return ( 
+        <div className="Screen">
+            <body>
+                <h1>Hello my friend</h1>
+            </body>
+        </div>
+    );
+}
