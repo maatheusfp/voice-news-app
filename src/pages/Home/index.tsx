58d8de1b15726a4284d4617d105f3c4e75af3d47
Matheus PInheiro <matheus.pinheirofer@gmail.com>
2022-10-03 16:40:59 -0300

    started to use alan AI

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useEffect } from "react";
+import alanBtn from '@alan-ai/alan-sdk-web';
 import './style.css';
 
+const alanKey = 'c69864d278fdcba6f95d0790c0aa79bb2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
 export const Home: React.FC =() => {
+
+
+    useEffect(() => {
+        alanBtn({
+            key:alanKey,
+/*             onCommand: ({ command }) => {
+                if(command === 'testCommand') {
+                    alert('The command was executed')
+                }
+            } */
+        })
+    }, [])
+
     return ( 
         <div className="Screen">
             <body>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Switch, Route } from 'react-router-dom';
+import {  Route, BrowserRouter } from 'react-router-dom';
 import { Home } from "./pages/Home";
 
 export const Routes = (): JSX.Element => {
     return(
-        <Switch>
+        <BrowserRouter>
             <Route path="/">
                 <Home />
             </Route>
-        </Switch>
+        </BrowserRouter>
     );
 }
